c7b3b3c5e518f137b247f8dc320d27c0e89ed65e
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-04-27 08:09:00 +0300

    Feat: api keys

---
+++ app/config.py
@@ -0,0 +1,5 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIES_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
---
+++ request.py
@@ -0,0 +1,4 @@
+from instance import config
+#Getting api key
+
+api_key = config['MOVIES_API_KEY']
